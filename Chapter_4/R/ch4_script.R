dc8cab02ce95beaa83d5f09b2059edea8a5cd145
rgriff23 <rgriff23@gmail.com>
2017-03-12 16:51:13 +0900

    add wireframe code

--- Chapter_4/R/ch4_script.R
+++ Chapter_4/R/ch4_script.R
@@ -4,9 +4,9 @@
 
 # load packages
 library(geomorph)
-library(ggplot2)
-library(plyr) # for 'mapvalues' function to create wireframe colors
-library(abind) # for reading in landmarks
+library(ggplot2) 
+library(plyr) # for 'mapvalues' function
+library(abind) # for formatting landmark array
 
 # load custom functions
 source('~/Desktop/GitHub/Dissertation/Chapter_4/R/functions/read.pp.R', chdir = TRUE)
@@ -26,21 +26,42 @@ for (i in 1:length(files)) {landmarks <- abind(landmarks, read.pp(files[i]), alo
 # compute standard deviations for each landmark/dimension 
 sds.fas <- sd.coords(landmarks[,,1:16])
 sds.nem <- sd.coords(landmarks[,,17:32])
-sds.fas
-sds.nem
 
 # nested ANOVA
 alignment <- factor(rep(rep(1:4, each=4), 2))
 specimen <- rep(c("M.fas", "M.nem"), each=16)
 landmarks.pca <- gpagen(landmarks)$coords
 mod <- procD.lm(landmarks.pca ~ specimen/alignment)
-mod
 
 #########
 # PLOTS #
 #########
 
-# distribution of coordinate standard deviations
+# define wireframe, colors, and mean shape
+wireframe <- read.csv("~/Desktop/GitHub/Dissertation/Chapter_4/data/wireframe.csv", header=F)
+modules <- c("mandible", "face", "braincase", "zygomatic", "basicranium")
+module_colors <- c("red", "green", "blue", "purple", "gold")
+wireframe$colors <- mapvalues(wireframe[,1], modules, module_colors)
+mean.fas <- gpagen(landmarks[,,1:16])$consensus
+mean.nem <- gpagen(landmarks[,,17:32])$consensus
+
+# 3D wireframes
+layout3d(matrix(1:2, 1, 2), sharedMouse = TRUE)
+plot3d(mean.fas, xlab="", ylab="", zlab="", size=1, col="darkgray", type='s', aspect=F, box=F, axes=F)
+bgplot3d({plot.new()
+  title(main=expression(italic("M. fascicularis")), line=2, cex.main=2.5)
+  legend("topright", title="Region", legend=modules, col=module_colors, lwd=2, cex=1.2, bty="n")})
+for (i in 1:nrow(wireframe)) {
+  segments3d(rbind(mean.fas[wireframe[i,2],], mean.fas[wireframe[i,3],]), lwd = 2, col=wireframe[i,4])
+}
+plot3d(mean.nem, xlab="", ylab="", zlab="", size=1, col="darkgray", type='s', aspect=F, box=F, axes=F)
+bgplot3d({plot.new()
+  title(main=expression(italic("M. nemestrina")), line=2, cex.main=2.5)})
+for (i in 1:nrow(wireframe)) {
+  segments3d(rbind(mean.nem[wireframe[i,2],], mean.nem[wireframe[i,3],]), lwd = 2, col=wireframe[i,4])
+}
+
+# histograms showing distribution of coordinate standard deviations
 hist_dat <- data.frame(sds=c(sds.fas, sds.nem), 
   sp=factor(rep(c("M. fascicularis","M. nemestrina"),each=26*3)),
   v=rep(c(median(sds.fas),median(sds.nem)),each=26*3))
@@ -53,19 +74,6 @@ ggplot(hist_dat, aes(sds)) +
   strip.text.x=element_text(face="italic", size=14),
   plot.margin=unit(c(1,1,0.5,0),"cm")) +
   geom_vline(aes(xintercept=v), linetype=2, size=1)
-
-# 3d scatterplot
-plotAllSpecimens(landmarks[,,1:16], mean=T, plot.param=list(pt.cex=0.5))
-
-# define wireframe
-wireframe <- read.csv("~/Desktop/GitHub/Dissertation/Chapter_4/data/wireframe.csv", header=F)
-
-# define wire colors
-wire_colors <- mapvalues(wireframe[,1], c("mandible", "face", "braincase", "zygomatic", "basicranium"), c("red", "green", "blue", "purple", "yellow"))
-
-# import landmark data
-test <- array(landmarks[,,1], dim=c(26,3,1))
-plotWireframe(test, wireframe[,2:3], link_col=wire_colors, bg_col="white")
 
 ########
 # END ##
