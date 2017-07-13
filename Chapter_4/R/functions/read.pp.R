6d2fa34fbc3246580f8973d27803797fd058e614
rgriff23 <rgriff23@gmail.com>
2017-07-13 15:42:35 +0900

    add NA handling

--- Chapter_4/R/functions/read.pp.R
+++ Chapter_4/R/functions/read.pp.R
@@ -1,19 +1,22 @@
 # function to read in pp files exported from Meshlab's PickPoints module
-# dim assumes 3D
-# round=TRUE rounds landmarks to 2 decimal places
-read.pp <- function (file, dim=3, round=TRUE) {
+# round=2 rounds landmarks to 2 decimal places
+read.pp <- function (file, round=2) {
 	file <- readLines(file)
 	lines <- file[grep("point", file)]
 	x <- strsplit(lines, "x=\"")
 	y <- strsplit(lines, "y=\"")
 	z <- strsplit(lines, "z=\"")
 	name <- strsplit(lines, "name=\"")
-	mat <- matrix(0, length(x), dim)
+	active <- strsplit(lines, "active=\"")
+	mat <- matrix(0, length(x), 3)
 	r <- c()
 	for (i in 1:length(lines)) {
-		mat[i,1] <- round(as.numeric(strsplit(x[[i]][2], "\"")[[1]][1]),2)
-		mat[i,2] <- round(as.numeric(strsplit(y[[i]][2], "\"")[[1]][1]),2)
-		mat[i,3] <- round(as.numeric(strsplit(z[[i]][2], "\"")[[1]][1]),2)
+		a <- as.numeric(strsplit(active[[i]][2], "\"")[[1]][1])
+		if (a==1) {
+			mat[i,1] <- round(as.numeric(strsplit(x[[i]][2], "\"")[[1]][1]), round)
+			mat[i,2] <- round(as.numeric(strsplit(y[[i]][2], "\"")[[1]][1]), round)
+			mat[i,3] <- round(as.numeric(strsplit(z[[i]][2], "\"")[[1]][1]), round)
+		} else {mat[i,1:3] <- rep(NA, 3)}
 		r <- c(r, strsplit(name[[i]][2], "\"")[[1]][1])
 	}
 	rownames(mat) <- r 
